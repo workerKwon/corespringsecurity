2064ab6bd66e755fcffe9c6026ba8e8f8cc5e232
workerKwon <dmdmdm1322@gmail.com>
2022-04-07 23:03:14 +0900

    로그인, 로그아웃 페이지 추가 및 컨트롤러 추가

--- src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -58,6 +58,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
 
         .and()
-                .formLogin();
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/login_proc")
+                .defaultSuccessUrl("/")
+                .permitAll();
     }
 }
