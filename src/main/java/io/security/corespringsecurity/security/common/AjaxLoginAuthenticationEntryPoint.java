82706e2e16748b27ee793faa20d8b9ad5afbadf2
workerKwon <dmdmdm1322@gmail.com>
2022-04-12 22:11:35 +0900

    인증 예외 처리 AuthenticationEntryPoint - 인증이 필요한 자원에 인증 없이 접근하려할 때 인가 예외 처리 AccessDeniedHandler - 인증은 했지만 접근 권한이 없을 때

---
+++ src/main/java/io/security/corespringsecurity/security/common/AjaxLoginAuthenticationEntryPoint.java
@@ -0,0 +1,18 @@
+package io.security.corespringsecurity.security.common;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AjaxLoginAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    // 익명 사용자가 인증이 필요한 자원에 접근했을 때 에러를 전달한다.
+   @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "UnAuthorized");
+    }
+}
---
+++ src/main/java/io/security/corespringsecurity/security/handler/AjaxAccessDeniedHandler.java
@@ -0,0 +1,21 @@
+package io.security.corespringsecurity.security.handler;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class AjaxAccessDeniedHandler implements AccessDeniedHandler {
+
+    /**
+     * 권한이 없을 경우 호출.
+     */
+    @Override
+    public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AccessDeniedException e) throws IOException, ServletException {
+        httpServletResponse.sendError(HttpServletResponse.SC_FORBIDDEN, "Access is denied");
+    }
+}
