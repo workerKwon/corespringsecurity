6a2796407823ee9e4fabd3b25db00908f17d84f5
workerKwon <dmdmdm1322@gmail.com>
2022-04-15 22:56:42 +0900

    SecurityMetadataSource - 사용자가 접근하고자 하는 URL 권한 정보 추출 - AccessDecisionManager 에게 권한 정보를 전달하여 인가처리 수행 - DB로부터 자원 및 권한 정보를 가져와서 매핑해서 관리하도록 구현한다. - 사용자의 매 요청마다 요청 정보에 매핑된 권한 정보를 확인해서 필요한 권한 정보를 Filter에게 전달 - Filter가 다시 AccessDecisionManager에게 전달하여 실질적인 인가 처리 진행

--- src/main/java/io/security/corespringsecurity/security/metadatasource/UrlFilterInvocationSecurityMetadataSource.java
+++ src/main/java/io/security/corespringsecurity/security/metadatasource/UrlFilterInvocationSecurityMetadataSource.java
@@ -10,15 +10,32 @@ import org.springframework.security.web.util.matcher.RequestMatcher;
 import javax.servlet.http.HttpServletRequest;
 import java.util.*;
 
+/**
+ * DB에서 가져온 자원 인가 정보를 담고, 유저 요청과 매칭해서 권한 정보를 반환하는 클래스
+ */
 public class UrlFilterInvocationSecurityMetadataSource implements FilterInvocationSecurityMetadataSource {
 
+
+    /**
+     * ConfigAttribute: 권한 정보 객체
+     */
     private LinkedHashMap<RequestMatcher, List<ConfigAttribute>> requestMap = new LinkedHashMap<>();
+
 
     @Override
     public Collection<ConfigAttribute> getAttributes(Object object) throws IllegalArgumentException {
 
+        /**
+         * 사용자가 요청하는 요청 객체
+         */
         HttpServletRequest request = ((FilterInvocation) object).getRequest();
 
+        // 임의로 준 url 자원에 따른 권한
+        requestMap.put(new AntPathRequestMatcher("/mypage"), Arrays.asList(new SecurityConfig("ROLUE_USER")));
+
+        /**
+         * 디비에서 가져온 URL 자원 인가 정보와 사용자 요청 정보가 매치되면 DB에서 가져온 권한 정보를 추출해서 반환
+         */
         if(requestMap != null){
             for(Map.Entry<RequestMatcher, List<ConfigAttribute>> entry : requestMap.entrySet()){
                 RequestMatcher matcher = entry.getKey();
